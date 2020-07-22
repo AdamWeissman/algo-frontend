ac1232bc5f547e922bf11304cad54a73f7014e3d
AdamWeissman <adam@comedytragedyepic.com>
2020-07-21 21:52:07 -0400

    definitely will have enough stateful components, and working on having enough stateless components... but may have stateless components that are pointless to fulfill requirement

--- src/components/EitherOrButton.js
+++ src/components/EitherOrButton.js
@@ -4,9 +4,9 @@ class EitherOrButton extends React.Component {
   render () {
     return (
       <div class="ui large buttons">
-        <button class="blue ui button active">FIRST</button>
+        <button class="blue ui button active">VIEW</button>
         <div class="or"></div>
-        <button class="black ui button">SECOND</button>
+        <button class="black ui button">ADD</button>
       </div>
   )
   }
