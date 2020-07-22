bb4f32ba548edd27cd69c64d9c4f4d2ee7b436fa
AdamWeissman <adam@comedytragedyepic.com>
2020-07-21 23:27:24 -0400

    setup a generic component that will determine whether or not render buttons

--- src/components/ExampleContentContainer.js
+++ src/components/ExampleContentContainer.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import ExampleContent from './ExampleContent'
+import ToShowOrNotToShow from './ToShowOrNotToShow'
 
 class ExampleContentContainer extends React.Component {
   render () {
     return (
       <center>
       <div className="circular ui segment" style={ {marginTop: '75px', marginLeft: '75px', marginRight: '75px'} }>
+        <center>
         <div className="circular ui inverted segment" style={{color: "#49fb35", marginTop: '0px', marginLeft: '50px', marginRight: '50px'} }>
           <center>
            <ExampleContent />
           </center>
         </div>
+        </center>
           <br />
+
           <center>
           <div className="circular ui inverted segment">
+            <ToShowOrNotToShow>
             <button class="circular ui inverted teal button">SAVE</button>
+            </ToShowOrNotToShow>
           </div>
           </center>
+          
       </div>
       </center>
     );
---
+++ src/components/ToShowOrNotToShow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EitherOrButton from './EitherOrButton'
+
+class ToShowOrNotToShow extends React.Component {
+  render () {
+    return (
+      <div>EITHER OR BUTTON OR SAVE BUTTON</div>
+    );
+  }
+}
+
+export default ToShowOrNotToShow;
