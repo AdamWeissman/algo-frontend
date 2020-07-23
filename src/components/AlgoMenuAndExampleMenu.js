f0ad8211110871e9796c856bbef83ab47569a8f7
AdamWeissman <adam@comedytragedyepic.com>
2020-07-23 14:13:30 -0400

    removed the commented out code now that it works in app

--- src/components/AlgoMenuAndExampleMenu.js
+++ src/components/AlgoMenuAndExampleMenu.js
@@ -8,41 +8,7 @@ import axios from 'axios';
 
 class AlgoMenuAndExampleMenu extends React.Component {
 
-  // state = {
-  //   algorithms: [],
-  //   examples: [],
-  //   example: []
-  // }
-
-  // componentDidMount () {
-  //   axios.get('http://localhost:3001/api/v1/algorithms/idxe')
-  //     .then(response => {
-  //         this.setState({algorithms: response.data});
-  //         console.log(response)
-  //     });
-  // }
-
-  // algoGetExamplesClickHandler = async(the_algorithm) => {
-  //   await axios.get(`http://localhost:3001/api/v1/algorithms/${the_algorithm}/examples`)
-  //     .then(response => {
-  //         this.setState({examples: response.data});
-  //         console.log(response)
-  //     });
-  // }
-
-  // examplesGetContentClickHandler = async(the_algorithm, the_example) => {
-  //   await axios.get(`http://localhost:3001/api/v1/algorithms/${the_algorithm}/examples/${the_example}`)
-  //     .then(response => {
-  //         this.setState({example: response.data});
-  //         console.log(response)
-  //     });
-  // }
-
-  
-
-
-
-  
+ 
   render () {
     return (
       <div className="ui segment" style={ {marginTop: '5px', marginLeft: '89px', marginRight: '89px'} }>
