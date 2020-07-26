a5646a1aea378498ec938801ec2740fa26cc20b5
AdamWeissman <adam@comedytragedyepic.com>
2020-07-26 14:44:26 -0400

    testing to better understand difference between onChange and onSubmit

--- src/components/Examples/CreateExampleTitle.js
+++ src/components/Examples/CreateExampleTitle.js
@@ -2,13 +2,17 @@ import React from 'react'
 
 class CreateExampleTitle extends React.Component {
 
+  onInputChange(event) {
+    console.log(event.target.value)
+  }
+
   render () {
     return(
       <div>
         <center>{this.props.algorithm}</center>
         <form className="ui form">
           <div className="field">
-          <input type="text" />
+          <input type="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
