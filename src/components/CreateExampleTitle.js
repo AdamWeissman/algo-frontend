945d4bbbd54ecf243a672becb55d006f3908d0a2
AdamWeissman <adam@comedytragedyepic.com>
2020-07-26 11:28:39 -0400

    lots of undo... back to drawing board with createExampleTitle

--- src/components/CreateExampleTitle.js
+++ src/components/CreateExampleTitle.js
@@ -1,43 +1,18 @@
 import React from 'react'
 
-class CreateExampleTitle extends React.Component {
+const createExampleTitle = (props) => {
 
-  //constructor(props) {
-    //super(props);
-    state = {
-      value: ''
-    };
-
-    handleChange = this.handleChange.bind(this);
-    handleSubmit = this.handleSubmit.bind(this);
-  // }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
- 
-  handleSubmit(props) {
-    // alert('A name was submitted: ' + this.state.value);
-    // event.preventDefault();
-    props.submitExampleTitle(props.algorithm, this.state.value)
-  }
-
-  // onSubmit={() => props.submitExampleTitle(props.algorithm, this.name)}
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+  return (
+    <div>
+      <input type="text"
+      name="title"
+      placeholder="Enter a title."
+      onSubmit={props.submitExampleTitle(`${props.algorithm}`, "title test")}>
+      </input>
+    </div>
+  )
 }
 
 //need to set up POST request here
 
-export default CreateExampleTitle;
+export default createExampleTitle;
