281eaf58e1d2c64a714e531eb9ef3b4a74de8fe4
AdamWeissman <adam@comedytragedyepic.com>
2020-07-26 11:38:24 -0400

    attempting generic onChange handler

--- src/components/CreateExampleTitle.js
+++ src/components/CreateExampleTitle.js
@@ -3,25 +3,30 @@ import React from 'react'
 class CreateExampleTitle extends React.Component {
 
   state = {
-    firstName: '',
-    lastName: '',
+    title: '',
+    content: '',
     /* Initialize all text fields with empty strings. */
   }
 
   onChange = e => this.setState({ [e.target.name]: e.target.value })
 
-
-
   render() {
+    const {
+      title, 
+      content
+    } = this.state
   
     return (
-    <div>
-      <input type="text"
-      value=""
-      placeholder="Enter a title."
-      onSubmit={() => this.props.submitExampleTitle(`${this.props.algorithm}`, this.props.value)}>
+    <form>
+      <input 
+        type="text"
+        name="title"
+        value={title}
+        onChange={this.onChange}
+        placeholder="Enter a title."
+        onSubmit={() => this.props.submitExampleTitle(`${this.props.algorithm}`, this.props.value)}>
       </input>
-    </div>
+    </form>
     );
   }
 }
