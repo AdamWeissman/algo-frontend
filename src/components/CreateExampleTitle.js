6e86401e8fe023399d95e0775c8bfcc55a08883e
AdamWeissman <adam@comedytragedyepic.com>
2020-07-26 12:08:44 -0400

    another angle

--- src/components/CreateExampleTitle.js
+++ src/components/CreateExampleTitle.js
@@ -1,34 +1,13 @@
 import React from 'react'
 
-class CreateExampleTitle extends React.Component {
+const createExampleTitle = (props) => {
 
-  state = {
-    title: ''
-    /* Initialize all text fields with empty strings. */
-  }
+  return(
+    <div>placeholder</div>
+  )
 
-  onChange = e => this.setState({ [e.target.name]: e.target.value })
-
-  render() {
-    const {
-      title
-    } = this.state
-  
-    return (
-    <form>
-      <input 
-        type="text"
-        name="title"
-        value={title}
-        onChange={this.onChange}
-        placeholder="Enter a title."
-        onSubmit={() => this.props.submitExampleTitle(`${this.props.algorithm}`, this.props.value)}>
-      </input>
-    </form>
-    );
-  }
 }
 
 //need to set up POST request here
 
-export default CreateExampleTitle;
+export default createExampleTitle;
