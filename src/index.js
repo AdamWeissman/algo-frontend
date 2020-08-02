f4cf08cccbc00afebcb682a43e55699c4cae8fc5
AdamWeissman <adam@comedytragedyepic.com>
2020-08-02 09:25:26 -0400

    continued prep for redux

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 
 import Navigation from './components/Navigation';
-import reducers from './reducers/'
+import reducers from './store/reducers/'
 
 const store = createStore(reducers, applyMiddleware(thunk)); // or... create a store.js and import it
 
