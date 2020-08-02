78aed41f54204e08663bb8f44f5e34acc3879148
AdamWeissman <adam@comedytragedyepic.com>
2020-08-02 10:01:43 -0400

    importing as controlMode from whichMode so I can identify where code is coming from

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-
+import controlMode from './whichMode'
 // actual reducer Import here (one for whichMode and one for allAlgorithms... and another for algosWithExamples)
 // actual reducer Import here etc (reducers should be in their own file)... 
 
@@ -7,5 +7,5 @@ import { combineReducers } from 'redux';
 //export below as root reducer
 //make line 9-12 its own constant, and then export it (possibly)
 export default combineReducers ({
-  placeholder: () => 'go away error message' 
+  controlMode
 });
--- src/store/reducers/whichMode.js
+++ src/store/reducers/whichMode.js
@@ -2,4 +2,25 @@ import * as actionTypes from '../actions';
 
 export default function controlMode( state = {
   mode: ""
-})
+}, action) {
+  switch(action.type) {
+    case actionTypes.WHICH_MODE_DEFAULT:
+      return {
+        ...state,
+        mode: ""
+      }
+    case actionTypes.WHICH_MODE_EXPLORE:
+      return {
+        ...state,
+        mode: "EXPLORE"
+      }
+    case actionTypes.WHICH_MODE_CREATE:
+    return {
+        ...state,
+        mode: "CREATE"
+      }
+
+    default: 
+      return state;
+  }
+}
