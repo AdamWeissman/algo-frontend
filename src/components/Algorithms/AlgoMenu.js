04461e9a59efb6dfcae3e476d238d2731e40df02
AdamWeissman <adam@comedytragedyepic.com>
2020-08-02 13:36:52 -0400

    refactored as render list

--- src/components/Algorithms/AlgoMenu.js
+++ src/components/Algorithms/AlgoMenu.js
@@ -1,16 +1,24 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import AlgoMenuItem from './AlgoMenuItem'
 
 class AlgoMenu extends Component {
-   render () {
-    const algorithms = this.props.algorithms.map(algorithm => {
+   
+  renderList() {
+    return this.props.algorithms.map(algorithm => {
       return <AlgoMenuItem key={algorithm.id} id={algorithm.id} algotype={algorithm.algotype} exampleGrabber={this.props.exampleGrabber} />
     });
-
+    
+  }
+  
+  
+  
+  render () {
+ 
     return (
       <div className="circular ui inverted segment" style={{color: "#49fb35", marginTop: '5px', marginBottom: '10px', marginLeft: '2px', marginRight: '5px', height: '300px'}}>
         <div className="circular" align="center" style={{ maxHeight: '175px', maxWidth: '175px', overflow: 'scroll'}}>
-          {algorithms}
+          {/* {algorithms} */}
+          {this.renderList()}
         </div>
       </div>
     )
