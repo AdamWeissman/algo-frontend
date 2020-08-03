bb7c4f755cf6dfec88f8a775ec053283ae1eb1b3
AdamWeissman <adam@comedytragedyepic.com>
2020-08-02 23:14:32 -0400

    workingon index reducers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
-import { selectAlgorithm } from '../actions/'
+// import { selectAlgorithm } from '../actions/'
 
 
 class App extends Component {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,33 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 
-export default combineReducers({
+const someAlgorithmsReducer = () => {
+  return [
+    { algotype: "SomeAlgo 1" },
+    { algotype: "SomeAlgo 2" },
+    { algotype: "SomeAlgo 3" },
+    { algotype: "SomeAlgo 4" },
+    { algotype: "SomeAlgo 5" },
+    { algotype: "SomeAlgo 6" }
+  ];
+};
 
-})
+const allAlgorithmsReducer = () => {
+  return [
+    { algotype: "AllAlgo 1" },
+    { algotype: "AllAlgo 2" },
+    { algotype: "AllAlgo 3" },
+    { algotype: "AllAlgo 4" },
+    { algotype: "AllAlgo 5" },
+    { algotype: "AllAlgo 6" }
+  ];
+};
+
+const algorithmSelectedReducer = (selectedAlgorithm=null, action) => {
+  if (action.type === 'ACTION_ALGORITHM_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedAlgorithm;
+};
+
+
